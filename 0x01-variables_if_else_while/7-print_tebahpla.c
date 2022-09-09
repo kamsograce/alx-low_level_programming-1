64ea04a2c79dcddbe0752b58f929dfe00378e9dc
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-09 23:34:55 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
