f24e728a10d9a54b73f04010396f51e2e475befb
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-09 23:37:26 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int i, a;
+
+for (i = 0; i < 9; i++)
+{
+for (a = i + 1; a < 10; a++)
+{
+putchar((i % 10) + '0');
+putchar((a % 10) + '0');
+
+if (i == 8 && a == 9)
+continue;
+
+putchar(',');
+putchar (' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
