fe025f08f00c32338ea8b79544b1f340d89c2bd7
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 20:06:14 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,9 +7,11 @@
 int main(void)
 {
 int number;
-for (number = 49; number < 57; number++)
+for (number = 48; number < 57; number++)
 {
 putchar(number);
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
