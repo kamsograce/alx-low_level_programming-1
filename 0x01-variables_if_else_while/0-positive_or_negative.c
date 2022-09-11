901862a23ae542b3b79283ec29a2e82415b5fcbc
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 17:24:29 +0100

    Determining if variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,32 +3,28 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point.
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-if (n == 0)
+f(n > 0)
 {
-printf("%i is zero\n", n);
+printf("%d is positive\n", n);
 }
-
-else if (n < 0)
-{printf("%i is negative\n", n);
+else if(n < 0)
+{
+printf("%d is negative\n", n);
 }
-
+else if(n == 0)
+{
+printf("%d is zero\n", n);
+}
 else
-{
-printf("%i is positive\n", n);
-}
-
+printf("Not a number\n");
 return (0);
-
 }
