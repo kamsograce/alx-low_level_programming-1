d8e7bb1a61077a476898c684b8a15d2eecbe08d3
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 19:21:54 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,12 @@
 int main(void)
 {
 int number;
-number = 0;
-while (number < 10)
-putchar();
+number = '0';
+while (number < '10')
+{
+putchar('number');
+number++;
+}
+putchar('\n')
 return (0);
 }
