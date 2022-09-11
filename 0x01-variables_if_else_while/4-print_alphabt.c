5f91a78bd9fe1b1477243e639f84a07fd186f5f3
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 18:44:11 +0100

    printing alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 char character;
 for (character  = 'a'; character <= 'z'; character++)
 {
-if (char != 'q' || char != 'e')
+if (character != 'q' || character != 'e')
 {
 putchar(character);
 }
