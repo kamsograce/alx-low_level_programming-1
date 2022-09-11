e68a668c7b220b0a571d887e366044bc18657dfe
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 18:13:36 +0100

    printing the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n%10 - 1 > 5)
 {
-printf("Last digit of %d is greater than 5", n)
+printf("Last digit of %d is greater than 5", n);
 }
 else if (n%10 - 1] == 0)
 {
-printf("Last digit of %d is 0",n)
+printf("Last digit of %d is 0",n);
 }
 else
 {
-printf("Last digit of %d is less than 6 and not 0",n)
+printf("Last digit of %d is less than 6 and not 0",n);
 }
 return (0);
 }
