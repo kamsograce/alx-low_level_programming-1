c7e2e83de1a2cacf24356742a0fe14a57241b586
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 18:39:21 +0100

    printing alphabets in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,6 +11,7 @@ char lowerCase, upperCase;
 for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 {
 putchar(lowerCase);
+}
 for (upperCase = 'a'; upperCase <= 'z'; upperCase++)
 {
 putchar(upperCase);
