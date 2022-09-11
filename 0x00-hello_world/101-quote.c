c713589dbd76913b31deb9433d9a08417a07fe48
Niyi <111027908+Gbeminiyi2022@users.noreply.github.com>
2022-09-11 07:37:09 +0100

    Printing to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(2, "and that piece of art is useful\" - Dora Korpar, 015-10-19\n");
+	fprintf(2, "and that piece of art is useful\" - Dora Korpar, 015-10-19");
 	return (1);
 }
