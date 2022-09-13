ae74144281d09f8c76d372035fb7ea52239f5fbe
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 18:15:34 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /*
- * print_alphabet_x10: print the alphabet 10 times
+ * print_alphabet_x10: prints the alphabet 10 times
  * followed by a new line
  * @void - no argument required
  * Description: function returns void and
  * has no argument
- * section header: It prints the alphabet in order as
- * specified by the count test, here it is 10.
  */
 void print_alphabet_x10(void)
 {
 char alphabet;
 int count = 0;
+/*We initialize the while loop*/
 while (count < 10)
 {
 alphabet = 'a';
