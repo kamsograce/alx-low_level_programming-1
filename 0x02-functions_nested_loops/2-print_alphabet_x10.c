c6392ecd20708bd7eb02b6473ab16dc554967bdc
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 18:27:51 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /*
- * print_alphabet_x10: prints the alphabet 10 times
+ * print_alphabet_x10: - prints the alphabet 10 times
  * @void - no argument required
  *
- * Description: function returns void
+ * Description: (function returns void)
  */
 
 void print_alphabet_x10(void)
