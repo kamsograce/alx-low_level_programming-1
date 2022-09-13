50ff322fe304ebfff4fb9f7cdf724ee83699117b
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 19:25:33 +0000

    printthe last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - computes value of last digit
+ * @x: integer value to be tested
+ *
+ * Description: using modulus operator
+ * Return: returns value of last digit
+ */
+int print_last_digit(int x)
+{
+return (x % 10);
+}
