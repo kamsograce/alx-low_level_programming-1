234550f0b42c35bc189985a437f4dd722892b505
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 09:47:55 +0000

    printing a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints the alphabet in lowercase
+ * followed by a new line.
+ * Return : void.
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+return;
+}
