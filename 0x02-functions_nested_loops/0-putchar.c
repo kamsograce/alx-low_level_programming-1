f15bdafc546e17e1ca74773d644ccabecd9bbea5
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 07:05:50 +0000

    Function to print a to z

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+void print_alphabet(void);
+
+int main()
+{
+	print_alphabet();
+	putchar('/n');
+	return (0);
+}	
+
+void print_alphabet(void)
+{
+	int number;
+	for(number =  97; number <= 122; number++)
+		putchar(number);
+	return;
+}
