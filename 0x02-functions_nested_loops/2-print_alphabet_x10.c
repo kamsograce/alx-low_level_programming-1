ee39d9d844be0bede21f6f658e379cb7843f3c42
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 18:09:17 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,6 +3,7 @@
  * print_alphabet_x10: print the alphabet 10 times
  * followed by a new line
  * @void - no argument required
+ *
  * Description: It prints the alphabet in order as
  * specified by the count test, here it is 10..
  */
