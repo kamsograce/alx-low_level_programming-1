1c616dd6c22d4b9abf131100c775101a94c44011
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 19:52:24 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,18 @@
  * print_last_digit - computes value of last digit
  * @x: integer value to be tested
  *
- * Description: using modulus operator
+ * Description: using modulus operator, then print
  * Return: returns value of last digit
  */
 int print_last_digit(int x)
 {
+int lastDigit;
 if (x >= 0)
-return (x % 10);
+;
 else
-return ((-1 * x) % 10);
+x = x * -1;
+
+lastDigit = x % 10;
+_putchar('0' + lastDigit);
+return (lastDigit);
 }
