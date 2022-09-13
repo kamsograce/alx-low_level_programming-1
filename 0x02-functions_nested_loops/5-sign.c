368caa4402089b73dd35f30d8740442788702213
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 17:39:58 +0000

    print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,21 @@
  * -1 if n < 0.
  */
 
-int print_sign(int n){
-if (n > 0){
+int print_sign(int n)
+ {
+if (n > 0)
+ {
 _putchar(43);
 return (1);
 }
-else if (n == 0){
+else if (n == 0)
+ {
 _putchar(48);
 return (0);
 }
-else{
+else
+ {
 _putchar(45);
 return (-1);
 }
-}
+} 
