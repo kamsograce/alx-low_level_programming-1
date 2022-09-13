72786b35322a7659353e22bba34443cf6e09621d
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-13 11:06:47 +0000

    Print alphabets a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * main - print 10 times the alphabet
+ * followed by a new line
+ * Return - void.
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+int count = 0;
+while (count < 10)
+{
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+count += 1;
+}
+return;
+}
