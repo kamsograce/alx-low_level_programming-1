f9f7c5d6efcc85f8739119d9c205ef05cf1fe7dc
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-14 17:39:37 +0000

    print from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print values from n to 98
+ * @n: The first value
+ * 
+ * Description: prints from n to 98, followed by a new line 
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int callNumber;
+	callNumber= n;
+	
+	printf("%d", callNumber);
+	if (callNumber == 98);
+	else if (callNumber < 98)
+	{
+		while (callNumber < 98)
+		{
+			callNumber++;
+			printf(", %d", callNumber);
+		}
+
+	}
+	else
+	{
+		while(callNumber > 98)
+		{
+			callNumber--;
+			printf(", %d", callNumber);
+		}
+	}
+	printf("\n");
+}
