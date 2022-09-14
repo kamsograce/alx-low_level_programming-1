2f2f19091734f232438b206f97c9cc77a50604b0
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-14 18:05:31 +0000

    sum of all factors of 3 or 5 less

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,13 +11,12 @@
 int main(void)
 {
 	int start, sum = 0;
-	
 	for (start = 0; start < 1024; start++)
 	{
 		if (start % 3 == 0 || start % 5 == 0)
 			sum += start;
 	}
 	printf ("%d\n", sum);
-	return(0);
+	return (0);
 }
 
