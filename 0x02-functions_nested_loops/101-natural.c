750f2632585895ba41ffbbfea3c370f2a8d4d0c2
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-14 18:01:25 +0000

    sum of all factors of 3 or 5 less

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the sum of all multiples of 3 or 5
+ * @void: void argument
+ *
+ * Description: prints the sum, followed by a new line
+ * Return: void
+ */
+
+int main()
+{
+	int start, sum = 0;
+	for (start = 0; start < 1024; start++)
+	{
+		if (start % 3 == 0 || start % 5 == 0)
+			sum += start
+	}
+	printf("%d\n", sum)
+	return(0);
+}
+
