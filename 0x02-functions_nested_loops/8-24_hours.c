51c01edc13e7040b6d75f4a068bd535362ced206
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-14 00:00:09 +0000

    print time from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * jack_bauer - displays time
- * @void: no argument to be passed 
+ * @void: no argument to be passed
  *
  * Description: prints time form "00:00" to "23:59"
  * Return: 0 Always
  */
 void jack_bauer(void)
 {
-	int second = 0, w = 0, x = 0, y = 0, z =0;
+	int second = 0, w = 0, x = 0, y = 0, z = 0;
+	
 	while (second < 1440)
 	{
 		_putchar(w + '0');
-		_putchar(x + '0'); 
-		_putchar(':'); 
+		_putchar(x + '0');
+		_putchar(':');
 		_putchar(y + '0');
 		_putchar(z + '0');
 		_putchar('\n');
@@ -36,5 +37,5 @@ void jack_bauer(void)
 		}
 		second++;
 	}
-	return;
+
 }
