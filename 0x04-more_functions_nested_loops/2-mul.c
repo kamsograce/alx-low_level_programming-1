4cd27b46fd6fb94321b1a86579e1daeb2c34de0a
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 06:49:29 +0000

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * mul - type int.
+ * @a: the multiplicand
+ * @b: the multiplier
+ * Description: multiples a and b.
+ * Return: the result.
+ */
+
+int mul(int a, int b)
+{
+		return (a * b);
+
+}
