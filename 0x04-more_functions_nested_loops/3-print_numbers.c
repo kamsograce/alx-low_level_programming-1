877ae42802963ccfec73994431ee05fcea7ed402
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 06:50:17 +0000

    print number 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * mul - type void.
+ * @void: no argument required
+ * 
+ * Description: prints numbers 0 to 9.
+ * Return: void.
+ */
+
+void print_numbers(void)
+{
+	int character;
+
+	for(character = 48; character <= 57; character++)
+		_putchar(character);
+	_putchar('\n');
+}
