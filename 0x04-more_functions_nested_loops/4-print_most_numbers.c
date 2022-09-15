cdbae498d3a515d3e62f3b6ef342dcf54931b08a
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 16:10:05 +0000

    print 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@
 
 void print_most_numbers(void)
 {
-	int character;
+int character;
 
-	for (character = 48; character <= 57; character++)
-	{
-		if (character != 50 && character != 52)
-			_putchar(character);
-	}
-	_putchar('\n');
+for (character = 48; character <= 57; character++)
+{
+if (character != 50 && character != 52)
+_putchar(character);
+}
+_putchar('\n');
 }
