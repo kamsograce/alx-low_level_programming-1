ce4f191b8de5fb330424a00cb67d9afa9a54bf61
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 11:40:11 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * print_size - type void.
+ * @size: size of the square
+ * 
+ * Description: prints a square, then a new line
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int count, counter;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (count = 1; count <= size; count++)
+		{
+			for (counter = 1; counter <= size; counter++) 
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}	
+
+	}
+}
