5864e36e862b6f9f8204a9064046326cfd2da359
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 04:23:02 +0000

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-  char string[] = "_putchar";
-  int index;
+	char string[] = "_putchar";
+	int index;
 
-  for (index = 0; index <= 7; index++)
-    _putchar(string[index]);
-  _putchar('\n');
+	for (index = 0; index <= 7; index++)
+   		 _putchar(string[index]);
+  	_putchar('\n');
   
   return (0);
 }
