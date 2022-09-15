69e2f326665c8ce87abfd0f35538058f0fa68e24
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 08:02:16 +0000

    print 0 to 14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * more_numbers - type void.
+ * @void: no argument required
+ * 
+ * Description: prints numbers 0 to 14 ten times.
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int character, counter = 0;
+
+	while (counter < 10)
+	{	
+		for (character = 48; character <= 57; character++)
+			_putchar(character);
+		for (character = 10; character <= 14; character++)
+		{
+			_putchar((character / 10) + '0');
+			_putchar((character % 10) + '0');
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
