2007e0e73f97be50b190c070c831fdbf2cac5b42
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 15:53:24 +0000

    print 0 - 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - type void.
+ * print_numbers - prints numbers 0 to 9
+ *
  * @void: no argument required
  * 
- * Description: prints numbers 0 to 9.
- * Return: return void.
+ * Return: Always void
  */
 
 void print_numbers(void)
@@ -12,6 +12,5 @@ void print_numbers(void)
 	int character;
 
 	for(character = 48; character <= 57; character++)
-		_putchar(character + '0');
-	_putchar('\n');
+		_putchar(character);
 }
