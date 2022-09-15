df4d0f0f830f4ae98102e77f7d333eda2368e0be
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 06:26:25 +0000

    check if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * _isdigit - type int.
+ * @c: character to check.
+ *
+ * Description: checks for a digit 0 through 9.
+ * Return: 1 if uppercase, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c > 46 && c < 58)
+		return (1);
+	else
+		return (0);
+
+}
