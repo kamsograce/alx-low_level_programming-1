5541b31340dbfb3a916b82e11fde0ff40b0b492c
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-15 11:34:29 +0000

    check if uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,8 +4,8 @@
  * _isupper - type int.
  * @c: character to check.
  *
- * Description: checks if character is an uppercase alphabet.
- * Return: 1 if uppercase, 0 otherwise.
+ * Description: checks if character is an upper.
+ * Return: returns 1 if uppercase, 0 otherwise.
  */
 
 int _isupper(int c)
