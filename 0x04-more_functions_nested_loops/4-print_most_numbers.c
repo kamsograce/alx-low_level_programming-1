29624b532a3f58bed8e15a54d438170dc019380e
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-16 04:11:34 +0000

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,19 @@
 /**
  * print_most_numbers - void
  * @void: no argument required
- * 
+ *
  * Description: prints 0 to 9
  * Return: returns void
  */
 
 void print_most_numbers(void)
 {
-int character;
+	int character;
 
-for (character = 48; character <= 57; character++)
-{
-if (character != 50 && character != 52)
-_putchar(character);
-}
-_putchar('\n');
+	for (character = 48; character <= 57; character++)
+	{
+		if (character != 50 && character != 52)
+			_putchar(character);
+	}
+	_putchar('\n');
 }
