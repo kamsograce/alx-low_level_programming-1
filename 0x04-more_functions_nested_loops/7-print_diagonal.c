4f0b35682c157f262e2ec030d689e2bf35f0b689
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-16 09:50:06 +0000

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - type void.
+ * print_diagonal - print
  * @n: number of times character should be printed
- *
- * Description: print the character '\', then a new line
- * Return: void.
  */
 
 void print_diagonal(int n)
@@ -13,21 +10,20 @@ void print_diagonal(int n)
 	int count, counter;
 
 	if (n <= 0)
-		;
+		_putchar('\n');
 	else
 	{
-		_putchar(92);
+		_putchar(92);	/* if n = 1*/
 		_putchar('\n');
-		for (count = 2; count <= n; count++)
+		for (count = 2; count <= n; count++) /*if n > 1*/
 		{
-			for (counter = 2; counter <= count; counter++)
+			for (counter = 2; counter <= count; counter++) /* for the spaces before /*/
 			{
 				_putchar(' ');
 			}
 			_putchar(92);
 			_putchar('\n');
 		}
-
 	}
-	_putchar('\n');
 }
+
