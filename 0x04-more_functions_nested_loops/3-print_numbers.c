d7730214004504b06ba351b7c2edcf86cd88be80
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-16 04:07:36 +0000

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_numbers - prints numbers 0 to 9
  * @void: no argument required
- * 
+ *
  * Description: same as above
  * Return: Always void
  */
@@ -11,6 +11,6 @@ void print_numbers(void)
 {
 	int character;
 
-	for(character = 48; character <= 57; character++)
+	for (character = 48; character <= 57; character++)
 		_putchar(character);
 }
