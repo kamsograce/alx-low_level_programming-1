6e4ade9936235d420c520d1c8eb8fe8c964f0411
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-16 04:46:20 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-/*
+/**
  * main - type int.
  * @void: no argument required
- * 
+ *
  * Description: print 1 - 100, if a multiple
  * of 3, print Fizz, a multiple of 5, print Buzz
  * else, print the number.
  * Return: void.
  */
 
-int main()
+int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
@@ -25,5 +26,5 @@ int main()
 			printf("%i", i);
 		printf(" ");
 	}
-	return 0;
+	return (0);
 }
