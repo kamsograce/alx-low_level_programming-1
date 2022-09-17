8d26ee4a5e59cbffcdd0354e3a3a05cccb1df038
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-17 12:33:56 +0000

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,24 +7,20 @@
  */
 int main(void)
 {
-int i, a;
+	int i, j;
 
-for (i = 0; i < 9; i++)
-{
-for (a = i + 1; a < 10; a++)
-{
-putchar((i % 10) + '0');
-putchar((a % 10) + '0');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar(j % 10 + '0');
+			if (i == 8 && j == 9)
+				continue;
 
-if (i == 8 && a == 9)
-continue;
-
-putchar(',');
-putchar (' ');
-}
-}
-
-putchar('\n');
-
-return (0);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
 }
