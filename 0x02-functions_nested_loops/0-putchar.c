b45f8c6448c83ac2b10d91b853ccf51ab6f13971
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-17 14:25:01 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,15 @@
 /**
  * main - prints the string "_putchar"
  * followed by a new line.
- * Return: void.
  */
-
 int main(void)
 {
 	char string[] = "_putchar";
 	int index;
 
 	for (index = 0; index <= 7; index++)
-		_putchar(string[index]);
+		_putchar(string + '0');
 	_putchar('\n');
+	
 	return (0);
 }
