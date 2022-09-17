27fca58c595ebc963252c2ce43aebe4e3f5931dd
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-17 14:37:22 +0000

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,15 @@
 /**
  * print_alphabet_x10 - Prints "a-z" ten times
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0, alphabet;
 
 	while (count < 10)
 	{
-		for (alphabet = 'a'; alphabet = 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 		_putchar('\n');
 		count++;
 	}
-	_putchar('\n');
 }
