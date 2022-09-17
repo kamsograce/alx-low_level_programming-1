ded1b95a04aeb0a834d5f332d395915be7b59a26
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-17 09:10:08 +0000

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,10 @@ int main(void)
 	char lowerCase, upperCase;
 
 	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
-	{
 		putchar(lowerCase);
-	}
 	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
-	{
 		putchar(upperCase);
-	}
 	putchar('\n');
+
 	return (0);
 }
