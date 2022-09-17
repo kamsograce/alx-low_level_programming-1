6e61dd3647c282a9e29011060a2ebe82653173c5
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-09-17 19:04:49 +0000

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,8 @@
  * mul - multiplies two integers.
  * @a: the multiplicand
  * @b: the multiplier
- * Return: 1 if True, 0 if false.
+ * Return: 1 if True,
+ * 0 if false.
  */
 
 int mul(int a, int b)
